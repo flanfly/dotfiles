143f901c91928521a353a1447a70b54629a65df7
Kai <seu@das-labor.org>
2026-02-07 15:48:38 +0000

    nvim: remove deprecated tree-sitter refactor

--- dot-config/nvim/lua/plugins/tree-sitter.lua
+++ dot-config/nvim/lua/plugins/tree-sitter.lua
@@ -1,30 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  init = function()
-    require("nvim-treesitter.query_predicates")
-  end,
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   opts = {
-    -- A list of parser names, or "all"
     ensure_installed = "all",
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- List of parsers to ignore installing (for "all")
     ignore_install = { "help", "ipkg" },
 
     highlight = {
-      -- `false` will disable the whole extension
       enable = true,
-
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser)
-      -- list of language that will be disabled
-      -- disable = {},
-      -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
       disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -33,7 +20,6 @@ return {
         end
       end,
 
-      -- disabled for catppuccino compatibility
       additional_vim_regex_highlighting = false,
     },
     incremental_selection = {
@@ -46,28 +32,6 @@ return {
     },
     indent = {
       enable = true,
-    },
-
-    -- treesitter-refactor config
-    refactor = {
-      highlight_definitions = {
-        enable = true,
-        -- Set to false if you have an `updatetime` of ~100.
-        clear_on_cursor_move = true,
-      },
-      highlight_current_scope = { enable = false },
-      smart_rename = {
-        enable = true,
-        keymaps = {
-          smart_rename = "gtr",
-        },
-      },
-      navigation = {
-        enable = true,
-        keymaps = {
-          goto_definition_lsp_fallback = "gd",
-        },
-      },
     },
 
     folds = {
